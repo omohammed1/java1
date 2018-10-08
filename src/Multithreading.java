300cbe1f949b7202491619109f42b5c9b066cdd9
omohammed1 <abotammam1@googlemail.com>
2018-10-09 00:39:38 +0300

    Second commit

---
+++ src/Multithreading.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Thread1 extends Thread {
+	private int counter;
+	public volatile boolean isRunning = true;
+	@Override
+	public void run() {
+		while(isRunning) {
+			System.out.println("Counter : " + counter);
+			counter++;
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
+public class Multithreading {
+
+	public static void main(String[] args) {
+		Thread1 t1 = new Thread1();
+		t1.start();
+		Scanner scanner = new Scanner(System.in);
+		scanner.nextLine();
+		t1.isRunning = false;
+	}
+
+}
